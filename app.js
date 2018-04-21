6af8566814f502e71192cc24077672a7cc7c3d52
Jimz <luisjc140992@gmail.com>
2018-04-21 15:11:51 -0500

    configuring database and product calculations

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 var shop = [];
+var count_prod = {};
 
 const products = sequelize.define('products', {
   code: Sequelize.STRING,
@@ -31,24 +32,115 @@ sequelize
   });
 
 
-app.get('/getAll', function (req, res) {
+app.get('/getAll', async (req, res) => {
 	
-	products.findAll().then(product => {
+	await products.findAll().then(product => {
+
 		return res.json(product)
-		})	
+		
+	})	
 })
 
-app.post('/getProduct', function (req, res) {
+app.post('/getProduct', async (req, res) => {
 	
-	let code = req.body.code;
-	shop.push(code)
+	let code = req.body.code;	
+
+	const product = await products.findAll({ where: { code: code } })
+
+	switch(product[0].code){
+		case 'PANTS':
+			
+			if ('PANTS' in count_prod) {
+
+				count_prod['PANTS'] += 1;
+				shop.push(product[0].code);
+
+			} else {
+
+				count_prod['PANTS'] = 1;
+				count_prod['PANTS-PRICE'] = product[0].price;
+				shop.push(product[0].code);
+
+			}
+			
+			break;
+		case 'TSHIRT':
+			
+			if ('TSHIRT' in count_prod) {
+
+				count_prod['TSHIRT'] += 1;
+				shop.push(product[0].code);
+
+			} else {
+
+				count_prod['TSHIRT'] = 1;
+				count_prod['TSHIRT-PRICE'] = product[0].price;
+				shop.push(product[0].code);
+
+			}
+			
+			break;
+		case 'HAT':
+
+			if ('HAT' in count_prod) {
+
+				count_prod['HAT'] += 1;
+				shop.push(product[0].code);
+
+			} else {
+
+				count_prod['HAT'] = 1;
+				count_prod['HAT-PRICE'] = product[0].price;
+				shop.push(product[0].code);
+
+			}
+
+			break;
+		default:
+			console.log('Not found')
+	}
 	
-	products.findAll({ where: { code: code } }).then(product => {
-		return res.json(product)
-		})
+	return res.json(product)
+		
+})
+
+app.use('/getPurchase', (req, res) => {
+	var r = 0;
 	
-	console.log(shop)
+	if (count_prod['PANTS']) {
 	
+		if (count_prod['PANTS'] > 1) {
+
+			r += (Math.floor(count_prod['PANTS'] / 2) * count_prod['PANTS-PRICE']) + count_prod['PANTS-PRICE'];
+		
+		} else {
+			
+			r += count_prod['PANTS'] * count_prod['PANTS-PRICE'];
+		
+		}
+	}
+	
+	if (count_prod['TSHIRT']) {
+	
+		if (count_prod['TSHIRT'] > 2) {
+		
+			r += count_prod['TSHIRT'] * 19;
+		
+		} else {
+		
+			r += count_prod['TSHIRT'] * count_prod['TSHIRT-PRICE'];
+		
+		}
+	}
+	
+	if (count_prod['HAT']) {
+	
+		if (count_prod['HAT']) {
+			r += count_prod['HAT'] * count_prod['HAT-PRICE'];
+		}
+	}
+	
+	return res.end('Items: '+shop+'\nTotal: $'+String(r))
 })
 
 sequelize
